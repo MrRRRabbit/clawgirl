f25f049cb1324f035df512574b58aa14e847365f
MrRRRabbit <zh593190@gmail.com>
2026-03-08 13:46:42 +0800

    Initial Commit

---
+++ ClawdAvatar/ClawdAvatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClawdAvatarApp.swift
+//  ClawdAvatar
+//
+//  Created by steve zhang on 2026/3/8.
+//
+
+import SwiftUI
+
+@main
+struct ClawdAvatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClawdAvatar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClawdAvatar
+//
+//  Created by steve zhang on 2026/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
